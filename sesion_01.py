2130fc17623f56f9fe88b4e599e97cafbbdc8627
Esaupro <nunezperezesauvladimir@gmail.com>
2026-02-10 19:12:34 -0800

    Guardando mi sesion de python basico

---
+++ sesion_01.py
@@ -0,0 +1,13 @@
+#Numerico y flotante
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.77))
+print(int(1+2))
+print(int(10*2))
+print("====operadores Matematicos======")
+# +
+# -
+# /
+# **
+# % Modulo
