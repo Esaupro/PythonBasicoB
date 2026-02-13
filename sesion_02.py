8dba41e0d9c94da54c19c595fd36e711111572c9
Esaupro <nunezperezesauvladimir@gmail.com>
2026-02-12 18:54:31 -0800

    el cambio en el archivo sesion_02.py

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1, 3.1416, "Esau", mi_Tupla]
 print("El primer elemento de mi lista: ",mi_lista[0])
 print("El cuarto elemento de mi lista: ",mi_lista[3])
 print("El tercer elemento de mi lista: ",mi_lista[2])
+
+#Diccionarios
+mi_Diccionario = {
+     "mi_lista": mi_lista,
+    "nombre": "Esau",
+    "Pi" : 3.1416,
+    "Tel": "355436555"
+}
+print("llave para accesar a mi diccionario mi_lista ",mi_Diccionario["mi_lista"])
+print("llave para accesar a mi diccionario mi_lista ",mi_Diccionario["Pi"])
+print("llave para accesar a mi diccionario mi_lista ",mi_Diccionario["Tel"])
