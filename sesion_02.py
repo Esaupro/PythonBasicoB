5dd2dfd375f7a64a433ee765861aa8314b21802e
Esaupro <nunezperezesauvladimir@gmail.com>
2026-02-12 18:40:27 -0800

    el cambio en el archivo sesion_02.py

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_Tupla = (2, 4)
+print("Mi tupla: ",mi_Tupla)
+
+#lista
+mi_lista = [1, 3.1416, "Esau", mi_Tupla]
+print("El primer elemento de mi lista: ",mi_lista[0])
+print("El cuarto elemento de mi lista: ",mi_lista[3])
+print("El tercer elemento de mi lista: ",mi_lista[2])
